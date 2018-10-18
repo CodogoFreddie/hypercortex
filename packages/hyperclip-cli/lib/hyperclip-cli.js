67cfaf1f6c3e7a3c091fd29437e41fe5e52275ae
Freddie Ridell <freddie@codogo.io>
2018-10-18 17:05:16 +0000

    inited package with lerna

---
+++ packages/hyperclip-cli/lib/hyperclip-cli.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = hyperclipCli;
+
+function hyperclipCli() {
+    // TODO
+}
---
+++ packages/hypercortex-cli/lib/hypercortex-cli.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = hypercortexCli;
+
+function hypercortexCli() {
+    // TODO
+}
