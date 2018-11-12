23dff60f911c0ae85fc0076888838d6df1364549
Freddie Ridell <contact@freddieridell.com>
2018-11-12 14:05:55 +0000

    removed unused packages

--- packages/hyperclip-cli/lib/hyperclip-cli.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = hyperclipCli;
-
-function hyperclipCli() {
-    // TODO
-}
--- packages/hypercortex-cli/lib/hypercortex-cli.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = hypercortexCli;
-
-function hypercortexCli() {
-    // TODO
-}
