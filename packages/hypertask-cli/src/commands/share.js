7ee4a61d00b8d522cc5661a54139c3f674f48012
Freddie Ridell <contact@freddieridell.com>
2019-01-28 17:11:35 +0000

    moved to discovery swarm, will this also repeatedly connect?

--- packages/hypertask-cli/src/commands/share.js
+++ packages/hypertask-cli/src/commands/share.js
@@ -1,21 +1,26 @@
-import createSwarm from "hyperdiscovery";
+var createSwarm = require("discovery-swarm");
+const portfinder = require("portfinder");
+var defaults = require("dat-swarm-defaults");
 
-const share = ({ db }) => {
+const share = async ({ db }) => {
 	console.log("sharing hypercortex");
 
-	const swarm = createSwarm(db, {
-		upload: true,
-		download: true,
-	});
+	var swarm = createSwarm(
+		defaults({
+			utp: false,
+			tcp: true,
+			maxConnections: 3,
+		}),
+	);
 
-	swarm.on("connection", function(peer, { host, port }) {
-		console.log(`connected to ${host}:${port}`);
-		swarm.on("close", function() {
-			console.log(`disconected from ${host}:${port}`);
-		});
-	});
+	const port = await portfinder.getPortPromise();
+	swarm.listen(port);
+	swarm.join(db.discoveryKey); // can be any id/name/hash
 
-	console.log("started sharing");
+	swarm.on("connection", (connection, { host, port }) => {
+		console.log(`found + connected to peer ${host}:${port}`);
+		connection.pipe(db.replicate({ live: false })).pipe(connection);
+	});
 };
 
 export default share;
