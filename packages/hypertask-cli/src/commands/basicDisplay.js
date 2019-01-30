42ff7172bff16ee591dd0963227fc015cb6f9c92
Freddie Ridell <contact@freddieridell.com>
2019-01-30 10:50:15 +0000

    highlight overdue tasks

--- packages/hypertask-cli/src/commands/basicDisplay.js
+++ packages/hypertask-cli/src/commands/basicDisplay.js
@@ -7,7 +7,16 @@ const basicDisplay = async ({ filter, modifications, taskAll, task }) => {
 
 	const taskObjs = await Promise.all(tasks.map(t => t.toJsObject()));
 
-	renderTable(taskObjs);
+	const nowString = new Date().toISOString();
+	R.pipe(
+		R.map(
+			R.when(
+				({ due }) => due && due < nowString,
+				R.assoc("textColor", 5),
+			),
+		),
+		renderTable,
+	)(taskObjs);
 };
 
 export default basicDisplay;
