516af858b4a4e6c62ac71cfced499fa692c864b4
Freddie Ridell <contact@freddieridell.com>
2019-03-02 10:02:02 +0000

    feat(hyperpin-cli): always try to save the URL, even with not internet connections

--- packages/hyperpin-cli/src/commands/add.js
+++ packages/hyperpin-cli/src/commands/add.js
@@ -9,31 +9,38 @@ const add = async ({ pin }, _, url) => {
 	const newID = getId(16);
 	const newPin = pin(newID);
 
-	const response = await fetch(url);
-	const raw = await response.text();
-
-	const $ = cheerio.load(raw);
-
-	await newPin.createdAtSet(new Date().toISOString());
 	await newPin.urlSet(url);
-	await newPin.authorSet(
-		$("meta[property='article:author']").attr("content"),
-	);
-	await newPin.canonicalSet($("link[rel='canonical']").attr("href"));
-	await newPin.descriptionSet($("meta[name='description']").attr("content"));
-	await newPin.iconSet($("link[rel='icon']").attr("sizes"));
-	await newPin.imageSet($("meta[property='og:image']").attr("content"));
-	await newPin.keywordsSet(
-		($("meta[name='keywords']").attr("content") || "")
-			.split(",")
-			.slice(0, 5),
-	);
-	await newPin.siteNameSet(
-		$("meta[property='og:site_name']").attr("content"),
-	);
-	await newPin.subjectSet($("meta[name='subject']").attr("content"));
-	await newPin.titleSet($("title").text());
-	await newPin.archivedSet(false);
+
+	try {
+		const response = await fetch(url);
+		const raw = await response.text();
+
+		const $ = cheerio.load(raw);
+
+		await newPin.createdAtSet(new Date().toISOString());
+		await newPin.authorSet(
+			$("meta[property='article:author']").attr("content"),
+		);
+		await newPin.canonicalSet($("link[rel='canonical']").attr("href"));
+		await newPin.descriptionSet(
+			$("meta[name='description']").attr("content"),
+		);
+		await newPin.iconSet($("link[rel='icon']").attr("sizes"));
+		await newPin.imageSet($("meta[property='og:image']").attr("content"));
+		await newPin.keywordsSet(
+			($("meta[name='keywords']").attr("content") || "")
+				.split(",")
+				.slice(0, 5),
+		);
+		await newPin.siteNameSet(
+			$("meta[property='og:site_name']").attr("content"),
+		);
+		await newPin.subjectSet($("meta[name='subject']").attr("content"));
+		await newPin.titleSet($("title").text());
+		await newPin.archivedSet(false);
+	} catch (e) {
+		console.error(e);
+	}
 
 	const renderedPin = await renderPin(newPin);
 	console.log(renderedPin);
