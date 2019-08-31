2bf91fd13ffec196a0b819a00c6b8a1cc82952c5
Freddie Ridell <contact@freddieridell.com>
2019-08-31 01:01:14 +0100

    more meta

--- js/src/App.js
+++ js/src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
+import { ThemeProvider } from "emotion-theming";
+import { corePallete, buildScheme } from "@freddieridell/little-bonsai-styles";
 
 import HypertaskContext, { useHyperTask } from "./HypertaskContext";
 import TaskList from "./TaskList";
 
 //{"Create":[{"SetProp":{"Description":"test"}}]}
 
+const theme = buildScheme(corePallete);
+
 const App = () => {
 	const { loading, tasks, runCommand } = useHyperTask();
 
 	return (
-		<HypertaskContext.Provider value={{ tasks, runCommand }}>
-			{!loading && <TaskList />}
-		</HypertaskContext.Provider>
+		<ThemeProvider theme={theme}>
+			<HypertaskContext.Provider value={{ tasks, runCommand }}>
+				{!loading && <TaskList />}
+			</HypertaskContext.Provider>
+		</ThemeProvider>
 	);
 };
 
--- js/src/TaskList/index.js
+++ js/src/TaskList/index.js
@@ -1,9 +1,64 @@
 import React from "react";
+import styled from "@emotion/styled";
+import * as R from "ramda";
+import { createShadow, calm } from "@freddieridell/little-bonsai-styles";
 
 import HypertaskContext from "../HypertaskContext";
 
-const Task = ({ task: { description } }) => {
-	return <div>{description}</div>;
+const TaskStyled = styled.div(
+	calm({
+		display: "flex",
+		flexDirection: "column",
+		padding: R.path(["theme", "size", "space", 2]),
+	}),
+);
+
+const TaskDescription = styled.h2(
+	calm({
+		borderBottomColor: R.path(["theme", "color", "symantic", "text"]),
+		borderBottomStyle: "solid",
+		borderBottomWidth: R.path(["theme", "size", "space", 1]),
+		color: R.path(["theme", "color", "symantic", "text"]),
+		fontSize: R.path(["theme", "size", "font", 3]),
+		fontWeight: "normal",
+		margin: 0,
+		padding: R.path(["theme", "size", "space", 2]),
+	}),
+);
+
+const TaskTags = styled.ul(
+	calm({
+		display: "flex",
+		margin: 0,
+		padding: 0,
+		margin: R.path(["theme", "size", "space", 2]),
+	}),
+);
+const TaskTag = styled.li(
+	calm({
+		marginLeft: R.path(["theme", "size", "space", 5]),
+	}),
+);
+
+const caplitalizeFirst = R.pipe(
+	R.split(""),
+	R.over(R.lensIndex(0), x => x.toUpperCase()),
+	R.join(""),
+);
+
+const Task = ({ task: { description, tags } }) => {
+	return (
+		<TaskStyled>
+			<TaskDescription>{caplitalizeFirst(description)}</TaskDescription>
+			{tags && (
+				<TaskTags>
+					{tags.map(tag => (
+						<TaskTag>{tag}</TaskTag>
+					))}
+				</TaskTags>
+			)}
+		</TaskStyled>
+	);
 };
 
 const TaskList = () => {
