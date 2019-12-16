9579d06392f9e5892f38f83ca50befb4c2f42341
Freddie Ridell <contact@freddieridell.com>
2019-12-16 21:17:13 +0000

    added shonky daemonisation

--- rust/hypertask_sync_server/src/main.rs
+++ rust/hypertask_sync_server/src/main.rs
@@ -1,7 +1,38 @@
+extern crate daemonize;
 extern crate hypertask_sync_server;
+
+use std::fs::File;
+
+use daemonize::Daemonize;
 
 use hypertask_engine::prelude::*;
 
 fn main() -> HyperTaskResult<()> {
-    hypertask_sync_server::start()
+    let stdout = File::create("/tmp/hypertask-sync-server.out").unwrap();
+    let stderr = File::create("/tmp/hypertask-sync-server.err").unwrap();
+
+    let daemonize = Daemonize::new()
+        .pid_file("/tmp/hypertask-sync-server.pid")
+        .chown_pid_file(true)
+        .stdout(stdout)
+        .stderr(stderr)
+        .exit_action(|| println!("Executed before master process exits"))
+        .privileged_action(|| "Executed before drop privileges");
+
+    match daemonize.start() {
+        Ok(_) => {
+            println!("Success, daemonized");
+
+            hypertask_sync_server::start()
+        }
+        Err(e) => {
+            eprintln!("Error, {}", e);
+
+            Err(
+                HyperTaskError::new(HyperTaskErrorDomain::Syncing, HyperTaskErrorAction::Run)
+                    .msg("error forking sync server")
+                    .from(e),
+            )
+        }
+    }
 }
